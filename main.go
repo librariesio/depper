e52bf8eb366c84335789a9d2be2a402ca4f8c0bb
Tieg Zaharia <tieg@tidelift.com>
2021-01-04 15:12:00 -0500

    Lint

--- main.go
+++ main.go
@@ -15,10 +15,13 @@ func main() {
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
 	c := cron.New()
-	c.AddFunc("* * * * *", func() { fmt.Println("Hello world from Depper") })
+	if _, err := c.AddFunc("* * * * *", func() { fmt.Println("Hello world from Depper") }); err != nil {
+		_ = fmt.Errorf("Error: %v", err)
+		os.Exit(1)
+	}
 	c.Start()
 
 	sig := <-s
 	signal.Stop(s)
-	fmt.Printf("\nCaught signal: %s.\n", sig)
+	fmt.Printf("Caught signal: %s.\n", sig)
 }

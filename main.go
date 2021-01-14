0f0d0dfeaae7f6ae7826c992ce3e3e407312221f
Matt Ellis <mellis@tidelift.com>
2021-01-14 11:48:57 -0500

    Add elm (#16)
    * Add elm
    
    * Add latest run
    
    * update for comment
    
    * update

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func createPipeline() *publishers.Pipeline {
 func (depper *Depper) registerIngestors() {
 	depper.registerIngestor(ingestors.NewRubyGems())
 	depper.registerIngestorStream(ingestors.NewNPM())
+	depper.registerIngestor(ingestors.NewElm())
 }
 
 func (depper *Depper) registerIngestor(ingestor ingestors.Ingestor) {

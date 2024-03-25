014843cd65c297266ddd5f0ce8cb9453c3c0a01c
John Bintz <johnbintz@tidelift.com>
2024-03-25 10:44:02 -0400

    Add docs for easier discovery as to how Depper works

--- ingestors/interfaces.go
+++ ingestors/interfaces.go
@@ -6,11 +6,17 @@ import (
 	"github.com/librariesio/depper/data"
 )
 
+// Regular ingestors provide an API we can poll for changes. This polling
+// is done on a regular schedule.
 type Ingestor interface {
 	Schedule() string
 	Ingest() []data.PackageVersion
 }
 
+// Streaming Ingestors continually pull new release information from a
+// persistent source. NPM is an example of this, as it provides a
+// CouchDB API endpoint from which we can continually pull new
+// package data.
 type StreamingIngestor interface {
 	Ingest(chan data.PackageVersion)
 }
